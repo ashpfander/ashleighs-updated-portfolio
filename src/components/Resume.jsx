2bd11da0127b9a2eaae5f5f4882b8aeef29e7d35
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2024-04-14 21:49:36 -0400

    switched resume to image to display and included download button

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,13 +1,15 @@
-import resumeFile from '../assets/UpdatedResume.pdf';
-import { Document, Page } from 'react-pdf';
-import "react-pdf/dist/esm/Page/TextLayer.css";
+import resumeFile from '../assets/UpdatedResume.jpg';
+import resumePDF from '../assets/UpdatedResume.pdf';
 
 function Resume() {
     return (
       <div className="container-fluid">
-        <Document file={resumeFile}>
-          <Page className="resume" pageIndex={0} />
-        </Document>
+        <div className="mt-5 text-center">
+          <a href={resumePDF} download="AshleighPfanderResume" target='_blank'>
+            <button className="p-3 gabarito-regular">Download Resume</button>
+          </a>
+          <img className="resume" src={resumeFile} alt="Ashleigh's Resume"></img>
+        </div>
       </div>
     );
   }

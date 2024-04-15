0f57b68353d1cdc311e243adbb2409a461a292e1
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2024-04-14 22:03:58 -0400

    updated error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,9 +1,18 @@
+import { useRouteError } from 'react-router-dom';
+
 function Error() {
-    return (
-      <div className="">
-        
-      </div>
-    );
-  }
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div className="text-center p-4">
+      <h1 className="kalam-bold">Oops!</h1>
+      <p className="gabarito-regular">Sorry, an unexpected error has occurred.</p>
+      <p className="gabarito-regular">
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+}
   
-  export default Error;
+export default Error;

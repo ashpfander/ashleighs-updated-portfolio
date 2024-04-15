1592faffade1a53c6653f10ea89a6e411600926b
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2024-04-14 20:40:56 -0400

    updated footer to actually be at the bottom of page regardless of content on page, need to figure out resume situation

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,7 +1,13 @@
+import resumeFile from '../assets/UpdatedResume.pdf';
+import { Document, Page } from 'react-pdf';
+import "react-pdf/dist/esm/Page/TextLayer.css";
+
 function Resume() {
     return (
-      <div className="">
-        
+      <div className="container-fluid">
+        <Document file={resumeFile}>
+          <Page className="resume" pageIndex={0} />
+        </Document>
       </div>
     );
   }

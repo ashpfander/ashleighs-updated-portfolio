4839105e340655d9dab6c5f06c4ecc513f540b33
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2024-04-15 22:10:50 -0400

    project cards have been stylized and edited nomad experience image to match same size as others

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,24 +1,28 @@
 function Project(props) {
-    return (
-      <div className="">
+  return (
+    <div className="container-fluid text-center pt-5 footer-padding">
+      <div className="row">
         {props.projects.map((project) => (
-              <div className="">
-                <img alt="Screenshot of application" className="" src={project.image} />
-                <div className="">
-                <h2 className="">
-                    {project.title}
-                  </h2>
-                  <h3 className="">
-                    {project.subtitle}
-                  </h3>
-                  <p className="">{project.description}</p>
-                  <a href={project.link} className="">Live Link</a>
-                  <a href={project.github} className="">GitHub Repo</a>
-                </div>
+          <div className="col-6 mb-4">
+            <div className="card">
+              <img alt="Screenshot of application" className="card-img-top" src={project.image} />
+              <div className="card-body">
+                <h2 className="kalam-bold olive-text">
+                  {project.title}
+                </h2>
+                <h3 className="gabarito-regular">
+                  <b>{project.subtitle}</b>
+                </h3>
+                <p className="gabarito-regular">{project.description}</p>
+                <a href={project.link} className=""><button className="p-3 me-3 gabarito-regular">Live App</button></a>
+                <a href={project.github} className=""><button className="p-3 gabarito-regular">GitHub Repo</button></a>
               </div>
-          ))}
+            </div>
+          </div>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
+}
   
-  export default Project;
+export default Project;

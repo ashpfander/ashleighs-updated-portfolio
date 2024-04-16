e0781db83522ea6ee5bff815cd295e41b8658e23
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2024-04-16 17:36:09 -0400

    fixed navbar toggle to show menu items when clicked

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import Navigation from './Navigation';
+import "bootstrap/js/src/offcanvas.js";
 
 function Header() {
   return (
@@ -8,14 +9,15 @@ function Header() {
         <h1 className="title kalam-bold pt-2">
           Ashleigh Pfander
         </h1>
-        <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <button className="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="sidebar offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div className="offcanvas offcanvas-end justify-content-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
           <div className="offcanvas-header">
-            <h2 className="offcanvas-title" id="offcanvasNavbarLabel">Ashleigh Pfander</h2>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            <h2 className="offcanvas-title kalam-bold text-white pt-2 ps-3" id="offcanvasNavbarLabel">Ashleigh Pfander</h2>
+            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
+          <hr className="border-white border-2"/>
           <div className="offcanvas-body justify-content-end">
             <Navigation />
           </div>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,8 +7,8 @@ function Navigation() {
 
   // Returns the HTML coding for the navigation links
   return (
-    <ul className="nav gabarito-regular fs-5">
-      <li className="nav-item">
+    <ul className="nav gabarito-regular fs-5 row">
+      <li className="nav-item col-md-12 col-lg">
         <Link to="/"
           // This is a conditional (ternary) operator that checks to see if the current page is "Home"
           // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
@@ -16,28 +16,28 @@ function Navigation() {
           Home
         </Link>
       </li>
-      <li className="nav-item">
+      <li className="nav-item col-md-12 col-lg">
         <Link to="/AboutMe"
           // Check to see if the currentPage is `About`, and if so we use the active link class. Otherwise, we set it to a regular link class.
           className={currentPage === '/AboutMe' ? 'nav-link active' : 'nav-link'}>
           About Me
         </Link>
       </li>
-      <li className="nav-item">
+      <li className="nav-item col-md-12 col-lg">
         <Link to="/Portfolio"
           // Check to see if the currentPage is `Portfolio`, and if so we use the active link class. Otherwise, we set it to a regular link class.
           className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}>
           Portfolio
         </Link>
       </li>
-      <li className="nav-item">
+      <li className="nav-item col-md-12 col-lg">
         <Link to="/Contact"
           // Check to see if the currentPage is `Contact`, and if so we use the active link class. Otherwise, we set it to a regular link class.
           className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}>
           Contact
         </Link>
       </li>
-      <li className="nav-item">
+      <li className="nav-item col-md-12 col-lg">
         <Link to="/Resume"
           // Check to see if the currentPage is `Resume`, and if so we use the active link class. Otherwise, we set it to a regular link class.
           className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}>

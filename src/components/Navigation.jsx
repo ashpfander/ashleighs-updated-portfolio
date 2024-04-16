93a1b6afc1391b95e5238dee6a02fe797252e346
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2024-04-16 17:46:29 -0400

    navbar closes when a page link is clicked

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,7 +8,7 @@ function Navigation() {
   // Returns the HTML coding for the navigation links
   return (
     <ul className="nav gabarito-regular fs-5 row">
-      <li className="nav-item col-md-12 col-lg">
+      <li className="nav-item col-md-12 col-lg" data-bs-dismiss="offcanvas">
         <Link to="/"
           // This is a conditional (ternary) operator that checks to see if the current page is "Home"
           // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
@@ -16,28 +16,28 @@ function Navigation() {
           Home
         </Link>
       </li>
-      <li className="nav-item col-md-12 col-lg">
+      <li className="nav-item col-md-12 col-lg" data-bs-dismiss="offcanvas">
         <Link to="/AboutMe"
           // Check to see if the currentPage is `About`, and if so we use the active link class. Otherwise, we set it to a regular link class.
           className={currentPage === '/AboutMe' ? 'nav-link active' : 'nav-link'}>
           About Me
         </Link>
       </li>
-      <li className="nav-item col-md-12 col-lg">
+      <li className="nav-item col-md-12 col-lg" data-bs-dismiss="offcanvas">
         <Link to="/Portfolio"
           // Check to see if the currentPage is `Portfolio`, and if so we use the active link class. Otherwise, we set it to a regular link class.
           className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}>
           Portfolio
         </Link>
       </li>
-      <li className="nav-item col-md-12 col-lg">
+      <li className="nav-item col-md-12 col-lg" data-bs-dismiss="offcanvas">
         <Link to="/Contact"
           // Check to see if the currentPage is `Contact`, and if so we use the active link class. Otherwise, we set it to a regular link class.
           className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}>
           Contact
         </Link>
       </li>
-      <li className="nav-item col-md-12 col-lg">
+      <li className="nav-item col-md-12 col-lg" data-bs-dismiss="offcanvas">
         <Link to="/Resume"
           // Check to see if the currentPage is `Resume`, and if so we use the active link class. Otherwise, we set it to a regular link class.
           className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}>

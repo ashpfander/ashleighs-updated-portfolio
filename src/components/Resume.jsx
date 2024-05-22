def2f31a93714772fc578fcadfc46f8286d8e52f
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.attlocal.net>
2024-05-22 10:59:18 -0400

    updated minor styling on resume page

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -9,7 +9,7 @@ function Resume() {
           <a href={resumePDF} download="AshleighPfanderResume" target='_blank'>
             <button className="col-lg-2 col-12 p-3 gabarito-regular">Download Resume</button>
           </a>
-          <img className="resume" src={resumeFile1} alt="Ashleigh's Resume - First Page"></img>
+          <img className="resume mt-5 mb-4" src={resumeFile1} alt="Ashleigh's Resume - First Page"></img>
           <img className="resume" src={resumeFile2} alt="Ashleigh's Resume - Second Page"></img>
         </div>
       </div>

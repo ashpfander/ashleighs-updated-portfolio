7ed6e45d2d97b02a02a2c2c7159422f985063288
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2024-10-22 10:55:59 -0400

    updated resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,6 +1,5 @@
-import resumeFile1 from '../assets/AshleighPfander-UpdatedResume1.png';
-import resumeFile2 from '../assets/AshleighPfander-UpdatedResume2.png';
-import resumePDF from '../assets/AshleighPfander-UpdatedResume.pdf';
+import resumeFile from '../assets/AshleighPfander-Resume.png';
+import resumePDF from '../assets/AshleighPfander-Resume.pdf';
 
 function Resume() {
     return (
@@ -9,8 +8,7 @@ function Resume() {
           <a href={resumePDF} download="AshleighPfanderResume" target='_blank'>
             <button className="col-lg-2 col-12 p-3 gabarito-regular">Download Resume</button>
           </a>
-          <img className="resume mt-5 mb-4" src={resumeFile1} alt="Ashleigh's Resume - First Page"></img>
-          <img className="resume" src={resumeFile2} alt="Ashleigh's Resume - Second Page"></img>
+          <img className="resume mt-5 mb-4" src={resumeFile} alt="Ashleigh's Resume"></img>
         </div>
       </div>
     );

dec2edf2b8d3cf08af463d88782fff3eb472a38a
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2024-10-22 14:31:22 -0400

    updated about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,13 +9,16 @@ function AboutMe() {
           </div>
           <div className="col-lg-9 col-md-12 footer-padding">
             <h2 className="gotu-regular olive-text">Hi! I'm Ashleigh.</h2>
-            <p>I am a recent Full Stack Web Development Bootcamp graduate with an interest in Front End Developing based on my past design experience. Eventually, I would love to get into the game developing world.</p>
-            <p>I recently left my job as a Graphic Designer at a screen printing shop to focus on pursuing a career in coding.
+            <p>I am a Graphic Designer, Illustrator, and most recently added Full-Stack Developer to my plate.
+            I mainly focus on typography, handlettering, cartoon/vintage style illustrations, and love doing Front-End Development. </p>
+            <p>I left my job as a Graphic Designer at a screen printing shop in May 2024 to focus on pursuing a career in coding.
             I originally acquired a Bachelor's Degree in Advertising & Graphic Design at CCAD.
-            After spending roughly 6 years at my company doing various graphic design work, I've come to the realization that this doesn't interest me anymore.
+            After spending roughly 6 years at my company doing various graphic design work, I wanted a change.
             After some thought into what other career path I could take, I remembered that I originally had wanted to be a Web Developer/Designer when I was much younger.
             Not wanting to go back to another 4 year college, I discovered bootcamps and read that they are just as good as getting a degree.
-            Now that I have finished my course, I'm excited and eager to get into the world of coding!</p>
+            I had some of the most fun learning how to code, but realized that I really need to practice my skills more before jumping into the career field.
+            After spending a good amount of the Summer and part of Fall not working, I also realized I missed doing design work.
+            I'm hoping I can land another awesome design job while also being able to learn more coding skills and be able to utilize them in the future.</p>
             <p>In my freetime, I love spending time with my husband, son, and two puppies. I also enjoy playing video games, watching shows and movies, coming up with a million things to do to my house, and napping (when I can).</p>
           </div>
           </div>

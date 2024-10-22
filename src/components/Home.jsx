c22606ad00deb66f21f7e2092f030b4265296d41
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2024-10-22 16:11:17 -0400

    added a typewriter effect to the homepage. need to figure out how to format it better

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,20 @@
+import Typewriter from 'typewriter-effect';
+
 function Home() {
   return (
     <div className="custom-container">
       <div className="background-image">
         <div className="floating-box pt-4 ps-4 pe-4 pb-3">
           <h2 className="gotu-regular"><b>Hello!</b> My name is <b>Ashleigh Pfander</b>.</h2>
-          <p className="gabarito-regular">I am a Mom, a Graphic Designer, and a Full Stack Web Developer.</p>
+          <p className="gabarito-regular typewriter-effect">I am a(n)&nbsp;
+            <Typewriter
+              options={{
+                strings: ['mom', 'Graphic Designer', 'Illustrator', 'Full-Stack Developer'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </p>
         </div>
       </div>
     </div>

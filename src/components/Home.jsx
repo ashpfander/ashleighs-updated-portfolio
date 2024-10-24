c09686b6bfff385eceb1130d1d57ef47c2948257
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2024-10-24 13:16:54 -0400

    updated header components to be white on the homepage, but olive on all other pages for contrast

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,42 @@
 import Typewriter from 'typewriter-effect';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 function Home() {
+  const location = useLocation();
+
+
+  useEffect(() => {
+    const links = document.querySelectorAll('.nav-link');
+    const title = document.querySelector('.title');
+    // Sets the header text color based on path
+    const getHeaderColor = () => {
+      if (location.pathname === '/') {
+        title.classList.add('title-home'); // Sets this class for the home path
+        links.forEach(link => {
+          link.classList.add('nav-link-home')
+        });
+        document.querySelector('header').style.borderBottom = 'none';
+      } else {
+        title.classList.remove('title-home'); // Removes this class on other pages
+        links.forEach(link => {
+          link.classList.remove('nav-link-home')
+        });
+        document.querySelector('header').style.borderBottom = '1px solid #80804d';
+      }
+    };
+
+    getHeaderColor();
+
+    return () => {
+      title.classList.remove('title-home'); // Reset class when component unmounts
+      links.forEach(link => {
+        link.classList.remove('nav-link-home');
+      });
+      document.querySelector('header').style.borderBottom = '';
+    };
+  }, [location]);
+
   return (
     <div className="custom-container">
       <div className="background-image">

afc2233edcf3bb7e1df3a44a20cdbfceea1e74fc
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2025-02-21 11:21:48 -0500

    updated social links to icons in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,34 @@
+import githubLogo from '../assets/github-logo.png';
+import linkedinLogo from '../assets/linkedin-logo.png';
+import instagramLogo from '../assets/instagram-logo.png';
+
 function Footer() {
     return (
       <footer className="p-2">
-        <div className="container-fluid d-flex justify-content-center">
-          <ul className="nav gabarito-regular fs-5 row text-center">
-            <li className="nav-item col-12 col-md">
-              <a className="nav-link" href="https://github.com/ashpfander">GitHub</a>
-            </li>
-            <li className="nav-item col-12 col-md">
-              <a className="nav-link" href="https://www.linkedin.com/in/ashleigh-pfander/">LinkedIn</a>
-            </li>
-            <li className="footer-item col-12 col-md">
-              <p>ashmpfander@gmail.com</p>
-            </li>
-            <li className="footer-item col-12 col-md">
-              <p>937.684.1677</p>
-            </li>
-          </ul>
+        <div className="container-fluid d-flex flex-column flex-lg-row justify-content-center">
+          <div className="footer-icons d-flex">
+            <ul className="nav">
+              <li className="nav-item col-sm">
+                <a className="nav-link" href="https://github.com/ashpfander"><img src={githubLogo} alt="Github Logo" className='footer-img'/></a>
+              </li>
+              <li className="nav-item col-sm">
+                <a className="nav-link" href="https://www.linkedin.com/in/ashleigh-pfander/"><img src={linkedinLogo} alt="LinkedIn Logo" className='footer-img'/></a>
+              </li>
+              <li className="nav-item col-sm">
+                <a className="nav-link" href="https://www.instagram.com/createdby.ashleigh"><img src={instagramLogo} alt="Instagram Logo" className='footer-img'/></a>
+              </li>
+            </ul>
+          </div>
+          <div className="d-flex flex-column flex-lg-row align-items-center">
+            <ul className="nav gabarito-regular fs-5 text-center">
+              <li className="footer-item">
+                <p>ashmpfander@gmail.com</p>
+              </li>
+              <li className="footer-item">
+                <p>937.684.1677</p>
+              </li>
+            </ul>
+          </div>
         </div>
       </footer>
     );
